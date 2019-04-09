1321dae2a1854488a33237b8018cfe78e86303e7
DPelagio <pelagio3@gmail.com>
2019-04-09 16:20:03 -0500

    Added query num hyperlinks

--- neo4jBD.py
+++ neo4jBD.py
@@ -1,3 +1,4 @@
+    
 #Daniel Pelagio
 #Luis Carrasco
 #Code to connect to a database at port bolt://localhost:7687 and queue a query
@@ -7,11 +8,12 @@ from neo4j import GraphDatabase #pip3 install neo4j
 uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=None)
 
-def print_friends(tx, name):
-    for record in tx.run("MATCH (n:Person)-[:KNOWS]->(m) "
-                         "WHERE n.name = {name} "
-                         "RETURN m.name", name=name):
+def print_num_hyperlinks(tx, id):
+    for record in tx.run("MATCH (n)-[:HYPERLINKS_TO]->() "
+    					 "with n, count(r) as num_hyperlinks"
+                         "WHERE n.nodeID = {id} "
+                         "RETURN n, num_hyperlinks", id=id):
         print(record["m.name"])
 
 with driver.session() as session:
-    session.read_transaction(print_friends, "Jose")
+    session.read_transaction(print_num_hyperlinks, "0")
